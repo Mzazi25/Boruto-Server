8c24b420b1e7588ea0fc018a6156e149ff870cc9
Mzazi25 <langat.caleb25@gmail.com>
2022-08-24 09:09:27 +0300

    tested allHeroes api response

--- BorutoServer/src/test/kotlin/com/example/ApplicationTest.kt
+++ BorutoServer/src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,23 +1,24 @@
 package com.example
 
-import io.ktor.server.routing.*
+import com.example.models.ApiResponse
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.callloging.*
-import org.slf4j.event.*
-import io.ktor.server.request.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 import com.example.plugins.*
+import com.example.repository.HeroRepository
+import io.ktor.client.call.*
+import io.ktor.util.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
 
 class ApplicationTest {
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
     @Test
-    fun testRoot() = testApplication {
+    fun `access root endpoint, assert correct information`() = testApplication {
         application {
             configureRouting()
         }
@@ -32,4 +33,77 @@ class ApplicationTest {
             )
         }
     }
+    @OptIn(InternalAPI::class)
+    @Test
+    fun `access all heroes endpoints, assert correct information`() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/boruto/heroes").apply {
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+            val expected = ApiResponse(
+                success = true,
+                message = "Ok",
+                prevPage = null,
+                nextPage = 2,
+                heroes = heroRepository.page1
+            )
+            val actual = Json.decodeFromString<ApiResponse>(this.body())
+            assertEquals(
+                expected= expected,
+                actual = actual
+            )
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
