651224967c8c9a27249d11b09839ee619aac0e47
Mzazi25 <langat.caleb25@gmail.com>
2022-09-10 09:17:03 +0300

    Updated API Responses

--- BorutoServer/src/main/kotlin/com/example/Application.kt
+++ BorutoServer/src/main/kotlin/com/example/Application.kt
@@ -7,7 +7,8 @@ fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused")
-fun Application.module() {
+fun Application.
+        module() {
     configureKoin()
     configureSerialization()
     configureMonitoring()
