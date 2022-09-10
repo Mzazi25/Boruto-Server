fda263a53e3b6c08ae5eaee28915a5bd13d2e86e
Mzazi25 <langat.caleb25@gmail.com>
2022-09-10 09:25:55 +0300

    updated API responses

--- BorutoServer/src/main/kotlin/com/example/models/ApiResponse.kt
+++ BorutoServer/src/main/kotlin/com/example/models/ApiResponse.kt
@@ -9,5 +9,6 @@ data class ApiResponse(
     val prevPage: Int? = null,
     val nextPage: Int? = null,
     val heroes: List<Hero> = emptyList(),
-    val lastUpdated: Long? = null
+    val lastUpdated: Long? = null,
+    val newUpdates: Long? = null
 )
